81cf414afb388e6082a4f2aa3ff9208511bc047b
sqrhead <fshelna98@gmail.com>
2026-02-24 23:20:21 +0100

    mod

---
+++ pathfinder.py
@@ -0,0 +1,18 @@
+from a_maze_ing import Cell
+from a_maze_ing import Maze
+
+class Pathfinder:
+    def __init__(self):
+
+        pass
+
+    def get_path(self, start: Cell, end: Cell) -> list[Cell]:
+        wall_points: int  = 2
+        free_cell: int  = 1
+        path: list[Cell] = []
+
+        for y in [-1, 1]:
+            ...
+
+        for x in [-1, 1]:
+            ...
